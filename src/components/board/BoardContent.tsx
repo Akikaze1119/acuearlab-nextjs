13537caa97b5aa47595c02c234229981a150a969
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-09-27 13:43:13 -0700

    feat: add bar-chart for score

--- src/components/board/BoardContent.tsx
+++ src/components/board/BoardContent.tsx
@@ -1,7 +1,8 @@
 import { auth } from '@clerk/nextjs/server';
 import { fetchRecords } from '@/lib/recordData';
 import AuthError from '@/components/ui/AuthError';
-import BoardTable from './BoardTable';
+import BarChart from '@/components/board/BarChart';
+import BoardTable from '@/components/board/BoardTable';
 
 const BoardContent = async () => {
   const { userId } = auth();
@@ -11,8 +12,9 @@ const BoardContent = async () => {
   const records = await fetchRecords(userId);
 
   return (
-    <div className='mx-4'>
-      <h1 className='text-2xl font-bold'>Records</h1>
+    <div className='mx-4 flex flex-col gap-10 mt-5'>
+      <h1 className='text-3xl font-bold text-primary text-center'>Records</h1>
+      <BarChart records={records} />
       <BoardTable records={records} />
     </div>
   );
