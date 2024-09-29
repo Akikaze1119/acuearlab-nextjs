4fdf5ad4b7d79f153d106413836fedf144d37162
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-09-28 19:55:53 -0700

    fix: fix button link in result page

--- src/components/result/ResultButtons.tsx
+++ src/components/result/ResultButtons.tsx
@@ -1,21 +1,14 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
 import { Button } from 'antd';
-import { HistoryOutlined, PlayCircleOutlined, ReloadOutlined } from '@ant-design/icons';
-import { useQuizContext } from '@/context/QuizContext';
+import { HistoryOutlined, PlayCircleOutlined } from '@ant-design/icons';
 
 const ResultButtons = () => {
-  const router = useRouter();
-  const { restartQuiz } = useQuizContext();
-
   return (
     <div className='flex justify-center gap-4 mt-6'>
-      <Button type='primary' onClick={restartQuiz} size={'large'} style={{ fontSize: '1.1rem' }}>
+      <Button type='primary' href='/game' size={'large'} style={{ fontSize: '1.1rem' }}>
         New Game
         <PlayCircleOutlined />
       </Button>
-      <Button onClick={() => router.push('/board')} size={'large'} style={{ fontSize: '1.1rem' }}>
+      <Button href='/board' size={'large'} style={{ fontSize: '1.1rem' }}>
         Record
         <HistoryOutlined />
       </Button>
--- src/context/QuizContext.tsx
+++ src/context/QuizContext.tsx
@@ -2,8 +2,6 @@
 'use client';
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { TResult, TQuiz } from '@/lib/definitions';
-import { useRouter } from 'next/navigation';
-import { revalidatePage } from '@/actions/revalidation';
 
 interface QuizContextProps {
   quizzes: TQuiz[];
@@ -13,27 +11,17 @@ interface QuizContextProps {
   setResults: (results: TResult[]) => void;
   setCurrentQuizIndex: (index: number) => void;
   incrementQuizIndex: () => void;
-  restartQuiz: () => Promise<void>;
 }
 
 const QuizContext = createContext<QuizContextProps | undefined>(undefined);
 
 export const QuizProvider = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
-
   const [quizzes, setQuizzes] = useState<TQuiz[]>([]);
   const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
   const [results, setResults] = useState<TResult[]>([]);
 
   const incrementQuizIndex = () => {
     setCurrentQuizIndex((prev) => prev + 1);
-  };
-
-  const restartQuiz = async () => {
-    setCurrentQuizIndex(0);
-    setResults([]);
-    revalidatePage('/game');
-    router.push('/game');
   };
 
   return (
@@ -46,7 +34,6 @@ export const QuizProvider = ({ children }: { children: ReactNode }) => {
         setResults,
         setCurrentQuizIndex,
         incrementQuizIndex,
-        restartQuiz,
       }}
     >
       {children}
